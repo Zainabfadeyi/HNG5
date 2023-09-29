27fc6266fa0f06215491d4c7daf6b425c68fb7c0
Zainab omolola fadeyi <zainabfadeyi@gmail.com>
2023-09-29 10:08:29 +0100

    hng5

---
+++ video-player/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
+from fastapi import File, UploadFile
+import os
+
+app = FastAPI()
+# @app.post("/upload/")
+# async def upload_video(file: UploadFile):
+#     # Save the uploaded video file to a directory on the server.
+#     with open(file.filename, "wb") as video_file:
+#         video_file.write(file.file.read())
+#     return {"message": "Video uploaded successfully"}
+
+# @app.get("/playback/{video_filename}", response_class=HTMLResponse)
+# async def render_video_player(video_filename: str):
+#     # Generate the URL for the uploaded video file.
+#     video_url = f"/path/to/uploaded/videos/{video_filename}"
+    
+#     # Render the HTML page with the video player.
+#     with open("video_player.html", "r") as html_file:
+#         template = html_file.read()
+#         rendered_template = template.replace("{{ video_url }}", video_url)
+#         return rendered_template
+@app.post("/upload/")
+async def upload_video(file: UploadFile):
+    # Define the path to the "videos" folder
+    videos_folder = os.path.join("video-player", "videos")
+    
+    # Ensure the "videos" folder exists; create it if it doesn't.
+    os.makedirs(videos_folder, exist_ok=True)
+    
+    # Save the uploaded video file to the "videos" folder.
+    video_path = os.path.join(videos_folder, file.filename)
+    with open(video_path, "wb") as video_file:
+        video_file.write(file.file.read())
+    
+    return {"message": "Video uploaded successfully"}
+
+@app.get("/playback/{video_filename}", response_class=HTMLResponse)
+async def render_video_player(video_filename: str, request: Request):
+    base_url = request.base_url
+    video_url = f"{base_url}videos/{video_filename}"  # Updated URL path
+
+    # Render the HTML page with the video player.
+    with open("../index.html", "r") as html_file:
+        template = html_file.read()
+        rendered_template = template.replace("{{ video_url }}", video_url)
+        return rendered_template
